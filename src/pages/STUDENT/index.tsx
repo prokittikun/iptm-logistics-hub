fb68d52ea4870c3ba38481b3376503dd24c0e057
prokittikun <pro.kittikun.2546@gmail.com>
2024-10-04 01:42:02 +0700

    Redirect STUDENT page to /STUDENT/announce instead of /STUDENT/equipment

--- src/pages/STUDENT/index.tsx
+++ src/pages/STUDENT/index.tsx
@@ -10,7 +10,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   return {
     redirect: {
       permanent: false,
-      destination: `/STUDENT/equipment`,
+      destination: `/STUDENT/announce`,
     },
     props: {},
   };
